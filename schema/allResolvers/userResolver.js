d2abb55348c0606d6b78fd8bc75d37e40ae3f900
MajodeQueen <majonakyonyi@gmail.com>
2025-02-13 10:50:47 +0300

    fix: cookies clearing

--- schema/allResolvers/userResolver.js
+++ schema/allResolvers/userResolver.js
@@ -95,7 +95,7 @@ const UserResolver = {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
           maxAge: 1000 * 60 * 60, // 1 hour,
-          sameSite: 'None',
+          sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         });
   
 
